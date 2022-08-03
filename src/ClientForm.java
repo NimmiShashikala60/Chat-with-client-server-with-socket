e237ab2ddde409d737041b1b1644378629f9e111
NimmiShashikala60 <nimmishashikala385@gmail.com>
2022-08-03 23:36:29 +0530

    new update

---
+++ src/ClientForm.java
@@ -0,0 +1,13 @@
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class ClientForm {
+    public TextArea textline;
+    public TextField textmasage;
+    public Label txtclient;
+
+    public void btnsendonaction(ActionEvent actionEvent) {
+    }
+}
---
+++ src/ServerForm.java
@@ -0,0 +1,13 @@
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class ServerForm {
+    public Label txtserver;
+    public TextArea textLine;
+    public TextField txtMasage;
+
+    public void btnsendonaction(ActionEvent actionEvent) {
+    }
+}
